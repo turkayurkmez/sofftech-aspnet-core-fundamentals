f73c250b77d6f7b92f8a3be9032ebe24cdf303fb
türkay ürkmez <turkay.urkmez@outlook.com>
2024-06-03 15:51:43 +0300

    e-shop project structure

---
+++ eshop/eshop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eshop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
