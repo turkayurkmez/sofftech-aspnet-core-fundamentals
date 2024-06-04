b304a9beca2511c72c4dd3d1cb8c65e0ff267d9f
türkay ürkmez <turkay.urkmez@outlook.com>
2024-06-04 10:00:44 +0300

    Products showing in main page

--- eshop/eshop.Entities/Product.cs
+++ eshop/eshop.Entities/Product.cs
@@ -13,6 +13,7 @@ namespace eshop.Entities
         public string? Description { get; set; }
         public decimal Price { get; set; }
         public double? Rating { get; set; }
+        public string ImageUrl { get; set; } = "https://cdn.dsmcdn.com/ty1018/product/media/images/prod/SPM/PIM/20231019/13/899e8cb7-16a0-3617-bae0-555eacd2e713/1_org.jpg";
 
     }
 }
--- eshop/eshop.Web/Controllers/HomeController.cs
+++ eshop/eshop.Web/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+﻿using eshop.Entities;
 using eshop.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -15,7 +16,16 @@ namespace eshop.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var products = new List<Product>()
+            {
+                new(){ Id=1, Name="Ürün A", Description="Ürün A Açıklaması", Price=10, Rating=4.6},
+                new(){ Id=2, Name="Ürün B", Description="Ürün B Açıklaması", Price=10, Rating=4.6},
+                new(){ Id=3, Name="Ürün C", Description="Ürün C Açıklaması", Price=10, Rating=4.6},
+                new(){ Id=4, Name="Ürün D", Description="Ürün D Açıklaması", Price=10, Rating=4.6},
+                new(){ Id=5, Name="Ürün E", Description="Ürün E Açıklaması", Price=10, Rating=4.6},
+
+            };
+            return View(products);
         }
 
         public IActionResult Privacy()
